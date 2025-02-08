cbdf0bfc6445a1a08b20586be30c7e56bb637170
Kushal <kushalobroy@gmail.com>
2025-02-09 02:05:56 +0530

    initial commit

---
+++ src/main/java/com/kushalobroy/apivault/ApivaultApplication.java
@@ -0,0 +1,13 @@
+package com.kushalobroy.apivault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApivaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApivaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kushalobroy/apivault/ApivaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kushalobroy.apivault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApivaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
